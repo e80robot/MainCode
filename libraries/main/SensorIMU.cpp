7e6e26fb8db752f227a84f051eacd299d476c6c2
Erik Meike <erik@yoerik.com>
2019-04-26 16:39:14 -0700

    combine imu .h files

--- libraries/main/SensorIMU.cpp
+++ libraries/main/SensorIMU.cpp
@@ -1,78 +1,32 @@
 #include "SensorIMU.h"
 #include "Printer.h"
-#include <MPU9250.h>
-#include <quaternionFilters.h>
-
 extern Printer printer;
 
 SensorIMU::SensorIMU(void)
-  : DataSource("rollIMU,pitchIMU,headingIMU,accelX,accelY,accelZ,gyroX,gyroY,gyroZ,magX,magY,magZ,time",
-               "float,float,float,float,float,float,float,float,float,float,float,float,long"), myIMU(0x68, Wire, 400000) {
+  : DataSource("rollIMU,pitchIMU,headingIMU,accelX,accelY,accelZ,magX,magY,magZ,time",
+               "float,float,float,float,float,float,float,float,float,long") {
 }
 
 void SensorIMU::init(void) {
   Serial.print("Initializing IMU... ");
-  myIMU.MPU9250SelfTest(myIMU.selfTest);
-  myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
-  myIMU.initMPU9250();
-  myIMU.initAK8963(myIMU.factoryMagCalibration);
-  myIMU.getAres();
-  myIMU.getGres();
-  myIMU.getMres();
-  Serial.print("IMU self test completed");
+  if(myIMU.begin()==IMU_SUCCESS){
+    Serial.println("done");
+  }
+  else{
+    Serial.println("failed");
+  }
   start_time = millis();
 }
 
 void SensorIMU::read(void) {
 
   // Get new data samples
-
-  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
-  {
-    myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
-
-    // Now we'll calculate the accleration value into actual g's
-    // This depends on scale being set
-    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes; // - myIMU.accelBias[0];
-    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes; // - myIMU.accelBias[1];
-    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes; // - myIMU.accelBias[2];
-
-
-    myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
-
-    // Calculate the gyro value into actual degrees per second
-    // This depends on scale being set
-    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
-    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
-    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
-
-    myIMU.readMagData(myIMU.magCount);  // Read the x/y/z adc values
-
-    // Calculate the magnetometer values in milliGauss
-    // Include factory calibration per data sheet and user environmental
-    // corrections
-    // Get actual magnetometer value, this depends on scale being set
-    myIMU.mx = (float)myIMU.magCount[0] * myIMU.mRes
-               * myIMU.factoryMagCalibration[0] - myIMU.magBias[0];
-    myIMU.my = (float)myIMU.magCount[1] * myIMU.mRes
-               * myIMU.factoryMagCalibration[1] - myIMU.magBias[1];
-    myIMU.mz = (float)myIMU.magCount[2] * myIMU.mRes
-               * myIMU.factoryMagCalibration[2] - myIMU.magBias[2];
-
-
-  }
-  float ax = myIMU.ax;
-  float ay = myIMU.ay;
-  float az = myIMU.az;
-
-  float gx = myIMU.gx;
-  float gy = myIMU.gy;
-  float gz = myIMU.gz;
-
-  float mx = myIMU.mx;
-  float my = myIMU.my;
-  float mz = myIMU.mz;
-
+  float ax = myIMU.readAccelX();
+  float ay = -myIMU.readAccelY(); // LSM303C does not use RHR :(
+  float az = myIMU.readAccelZ();
+  float mx = myIMU.readMagX();
+  float my = -myIMU.readMagY(); // LSM303C does not use RHR :(
+  float mz = myIMU.readMagZ();
 
   // Record current time
   current_time = millis() - start_time;
@@ -81,10 +35,6 @@ void SensorIMU::read(void) {
   state.accelX = ax - accel_offsets[0];
   state.accelY = ay - accel_offsets[1];
   state.accelZ = az - accel_offsets[2];
-
-  state.gyroX = gx;
-  state.gyroY = gy;
-  state.gyroZ = gz;
 
   // Remove offsets from magnertometer measurements (base values in uTesla)
   mx = mx - mag_offsets[0];
@@ -96,19 +46,8 @@ void SensorIMU::read(void) {
   state.magY = mx * mag_ironcomp[1][0] + my * mag_ironcomp[1][1] + mz * mag_ironcomp[1][2];
   state.magZ = mx * mag_ironcomp[2][0] + my * mag_ironcomp[2][1] + mz * mag_ironcomp[2][2];
 
-  myIMU.mx = state.magX;
-  myIMU.my = state.magY;
-  myIMU.mz = state.magZ;
-
-  myIMU.updateTime();
-
-  myIMU.count = millis();
-  myIMU.sumCount = 0;
-  myIMU.sum = 0;
-
   // populate the roll, pitch, yaw with simple orientation calcs  
-  getOrientation();
-  myIMU.delt_t = millis() - myIMU.count;
+  getOrientation(state.accelX,state.accelY,state.accelZ,state.magX,state.magY,state.magY);
 }
 
 String SensorIMU::printRollPitchHeading(void) {
@@ -137,16 +76,6 @@ String SensorIMU::printAccels(void) {
   printString += " accelZ: ";
   printString += String(state.accelZ);
   printString += "[mg]";
-
-  printString += " gyroX: ";
-  printString += String(state.gyroX);
-  printString += "[deg/sec], ";
-  printString += " gyroY: ";
-  printString += String(state.gyroY);
-  printString += "[deg/sec], ";
-  printString += " gyroZ: ";
-  printString += String(state.gyroZ);
-  printString += "[deg/sec]";
   return printString;
 
   /*
@@ -171,44 +100,57 @@ size_t SensorIMU::writeDataBytes(unsigned char * buffer, size_t idx) {
   data_slot[3] = state.accelX;
   data_slot[4] = state.accelY;
   data_slot[5] = state.accelZ;
-  data_slot[6] = state.gyroX;
-  data_slot[7] = state.gyroY;
-  data_slot[8] = state.gyroZ;
-  data_slot[9] = state.magX;
-  data_slot[10] = state.magY;
-  data_slot[11] = state.magZ;
-  idx += 12*sizeof(float);
+  data_slot[6] = state.magX;
+  data_slot[7] = state.magY;
+  data_slot[8] = state.magZ;
+  idx += 9*sizeof(float);
   long * long_slot = (long *) (buffer + idx);
   long_slot[0] = current_time;
   return idx + sizeof(long);
 }
 
 
-void SensorIMU::getOrientation() {
+void SensorIMU::getOrientation(float ax, float ay, float az, float mx, float my, float mz) {
   // copied from Adafruit_Simple_AHRS
-    MadgwickQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
-                         myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
-                         myIMU.mx, myIMU.mz, myIMU.deltat);
+  
+  float const PI_F = 3.14159265F;
 
-        myIMU.yaw   = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ()
-                                * *(getQ() + 3)), *getQ() * *getQ() + * (getQ() + 1)
-                        * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) - * (getQ() + 3)
-                        * *(getQ() + 3));
-    myIMU.pitch = -asin(2.0f * (*(getQ() + 1) * *(getQ() + 3) - *getQ()
-                                * *(getQ() + 2)));
-    myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ() + 1) + * (getQ() + 2)
-                                * *(getQ() + 3)), *getQ() * *getQ() - * (getQ() + 1)
-                        * *(getQ() + 1) - * (getQ() + 2) * *(getQ() + 2) + * (getQ() + 3)
-                        * *(getQ() + 3));
+  // roll: Rotation around the X-axis. -180 <= roll <= 180                                          
+  // a positive roll angle is defined to be a clockwise rotation about the positive X-axis          
+  //                                                                                                
+  //                    ay                                                                           
+  //      roll = atan2(----)                                                                         
+  //                    az                                                                           
+  //                                                                                                                                      
+  state.roll = (float)atan2(ay, az);
 
-  state.roll = myIMU.roll;
-  state.pitch = myIMU.pitch;
-  state.heading = myIMU.yaw;
+  // pitch: Rotation around the Y-axis. -180 <= roll <= 180                                         
+  // a positive pitch angle is defined to be a clockwise rotation about the positive Y-axis         
+  //                                                                                                
+  //                                 -ax                                                             
+  //      pitch = atan(---------------------------------)                                             
+  //                    ay * sin(roll) + az * cos(roll)                                               
+  //                                                                                                                                  
+  if (ay * sin(state.roll) + az * cos(state.roll) == 0)
+    state.pitch = ax > 0 ? (PI_F / 2) : (-PI_F / 2);
+  else
+    state.pitch = (float)atan(-ax / (ay * sin(state.roll) + az * cos(state.roll)));
 
-  float PI_F = 3.14159;
+  // heading: Rotation around the Z-axis. -180 <= roll <= 180                                       
+  // a positive heading angle is defined to be a clockwise rotation about the positive Z-axis       
+  //                                                                                                
+  //                                       mz * sin(roll) - my * cos(roll)                            
+  //   heading = atan2(------------------------------------------------------------------------------)  
+  //                    mx * cos(pitch) + my * sin(pitch) * sin(roll) + mz * sin(pitch) * cos(roll))   
+  //                                                                                                                                   
+  state.heading = -(float)atan2(mz * sin(state.roll) - my * cos(state.roll), \
+                               mx * cos(state.pitch) + \
+                               my * sin(state.pitch) * sin(state.roll) + \
+                               mz * sin(state.pitch) * cos(state.roll));
+
   // convert to degrees
   state.roll *= 180.0/PI_F;
   state.pitch *= 180.0/PI_F;
-  state.heading *= 180.0/PI_F + 10.5;
+  state.heading *= 180.0/PI_F;
   
 }
